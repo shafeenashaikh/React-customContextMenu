4d5b6389f62cd289411c9e411680213ba2704619
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-08 19:45:35 +0530

    styled DataList using react styled-component library

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import List from './data';
+import styled from 'styled-components'
 
 class Main extends Component {
 
@@ -13,7 +14,8 @@ class Main extends Component {
         return (
             <div>
                 {
-                    List.map(item=>(
+                List.map(item=>(
+                    <ItemListContainer>
                         <div>
                             <div>
                                 {item.id}
@@ -22,12 +24,34 @@ class Main extends Component {
                                 {item.first_name}
                             </div>
                         </div>
-                        
-                    ))
-                }
+                    </ItemListContainer>
+                ))}
             </div>
         );
     }
 }
 
 export default Main;
+
+const AppContainer=styled.div`
+position:relative;
+  display:grid;
+  place-items:center;
+`
+
+const ItemListContainer=styled.div`
+display:flex;
+justify-content:space-evenly;
+align-items:center;
+border:1px solid #888;
+width:200px;
+border-radius:12px;
+margin:10px;
+`
+const ID=styled.div`
+margin:10px 20px; 
+border-right:1px solid #888;
+padding:0 10px;
+`
+const Name=styled.div`
+width:150px;`
